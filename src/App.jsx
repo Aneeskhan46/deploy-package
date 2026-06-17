4d241248698e08173dcdf4f47aad160bd1b833a6
Aneeskhan46 <aneeskhan86305@gmail.com>
2026-06-17 15:27:36 +0530

    addding

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { CustomMathEditor } from 'my-anees-package';
+import 'my-anees-package/style.css'; // if they need the styles
+
+function App() {
+  return (
+    <div>
+      <h2>Testing the package </h2>
+      <br></br>
+       <CustomMathEditor />
+    </div>
+  )
+}
+
+export default App;

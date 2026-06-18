6802931794533c424921fe6971e76208ce7fd606
Aneeskhan46 <aneeskhan86305@gmail.com>
2026-06-18 17:42:59 +0530

    addding

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import { CustomMathEditor } from 'my-anees-package';
-import 'my-anees-package/style.css'; // if they need the styles
+import 'my-anees-package/style.css'; 
 
 function App() {
+  const [editorValue, setEditorValue] = useState("");
+
   return (
     <div>
-      <h2 style={{color:"red",fontWeight:"bold",textAlign:"center"}}>Anees Testing the package </h2>
-      <br></br>
-       <CustomMathEditor />
+      <h2 style={{color:"red",fontWeight:"bold",textAlign:"center"}}>
+        Anees Testing the package 
+      </h2>
+      <br />
+      
+      {/* Pass the value and the onChange handler! */}
+      <CustomMathEditor 
+        value={editorValue} 
+        onChange={(newValue) => setEditorValue(newValue)} 
+      />
+      
     </div>
   )
 }
